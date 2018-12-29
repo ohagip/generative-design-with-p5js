9ddf84f167ca7646026753e136624407416d81f1
ohagip <ohagiohagiohagi@gmail.com>
2018-12-30 02:25:58 +0900

    Add p4-3-3.

---
+++ p4/p4-3-3/sketch.js
@@ -0,0 +1,79 @@
+let video;
+let x;
+let y;
+let curvePointX = 0;
+let curvePointY = 0;
+let pointCount = 1;
+let diffusion = 50;
+let streamReady = false;
+
+function preload() {
+  img = loadImage('data/pic.png');
+}
+
+function setup() {
+  createCanvas(640, 480);
+  background(255);
+  x = width / 2;
+  y = height / 2;
+  video = createCapture(VIDEO, function() {
+    streamReady = true;
+  });
+  video.size(width * pixelDensity(), height * pixelDensity());
+  video.hide();
+  noFill();
+}
+
+function draw() {
+  if (streamReady === true) {
+    for (let j = 0; j <= mouseX / 50; j += 1) {
+
+      // Retrieve color from capture device
+      let c = color(video.get(width - x, y));
+
+      // convert color c to HSV
+      let cHSV = chroma(red(c), green(c), blue(c));
+      strokeWeight(cHSV.get('hsv.h') / 50);
+      stroke(c);
+
+      diffusion = map(mouseY, 0, height, 5, 100);
+
+      beginShape();
+      curveVertex(x, y);
+      curveVertex(x, y);
+
+      for (let i = 0; i < pointCount; i += 1) {
+        let rx = int(random(-diffusion, diffusion));
+        curvePointX = constrain(x + rx, 0, width - 1);
+        let ry = int(random(-diffusion, diffusion));
+        curvePointY = constrain(y + ry, 0, height - 1);
+        curveVertex(curvePointX, curvePointY);
+      }
+
+      curveVertex(curvePointX, curvePointY);
+      endShape();
+
+      x = curvePointX;
+      y = curvePointY;
+    }
+  }
+}
+
+function keyReleased() {
+  if (key === 's' || key === 'S') {
+    saveCanvas('canvas', 'png');
+  }
+
+  if (key === 'q' || key === 'Q') {
+    noLoop();
+  }
+  if (key === 'w' || key === 'W') {
+    loop();
+  }
+  if (keyCode === UP_ARROW) {
+    pointCount = min(pointCount + 1, 30);
+  }
+  if (keyCode === DOWN_ARROW) {
+    pointCount = max(pointCount - 1, 1);
+  }
+}
